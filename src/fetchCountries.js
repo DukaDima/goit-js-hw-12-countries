9fd3c9b7e5f18952dc10d94ca121acddb1c998d1
Dima <75967292+DukaDima@users.noreply.github.com>
2021-06-26 20:17:43 +0300

    Делает запрос на бекенд

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+export default function fetchCountries(searchQuery) {
+return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(countries => {
+        console.log(countries)
+    })
+}
+
+
+
+
+    
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import './sass/main.scss';
 console.log('homework 12')
+import fetchCountries from './fetchCountries.js';
+
+const refs = {
+    input: document.querySelector('input'),
+}
+
+console.log(refs.input)
+var debounce = require('lodash.debounce');
+
+let searchQuery = ''
+refs.input.addEventListener('input', onInput)
+
+function onInput(e) {
+  
+   debounce((e) => {
+          e.preventDefault()
+    searchQuery=refs.input.value
+        console.log(refs.input.value)
+        console.log('debounce')
+    return `${searchQuery}`
+     
+    },500)
+ 
+  
+}
+
+const Countries = fetchCountries(searchQuery)
+console.log(Countries)
+
