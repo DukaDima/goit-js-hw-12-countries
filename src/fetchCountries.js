dd3b5c52202c2c8da30f3e43372be652d966357a
Dima <75967292+DukaDima@users.noreply.github.com>
2021-06-27 20:05:19 +0300

    делает запрос, дебоунс, проверку

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,14 @@
+//делает запрос на бекенд------------
+
+
 export default function fetchCountries(searchQuery) {
 return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
     .then(response => {
-        return response.json();
-    })
-    .then(countries => {
+        const countries = response.json()
         console.log(countries)
+        return countries;
     })
+    .catch(error=>console.log('error'))
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,31 +1,109 @@
+// список импорта------------------------------
 import './sass/main.scss';
-console.log('homework 12')
 import fetchCountries from './fetchCountries.js';
+import { alert, defaultModules } from '../node_modules/@pnotify/core';
 
+
+// ссылки на разметку------------------------------
 const refs = {
     input: document.querySelector('input'),
+    body: document.querySelector('body'),
+    country: document.querySelector('container'),
 }
-
 console.log(refs.input)
-var debounce = require('lodash.debounce');
+console.log(refs.body)
+console.log(refs.country)
+
+const debounce = require('lodash.debounce');
 
 let searchQuery = ''
-refs.input.addEventListener('input', onInput)
+console.log(searchQuery)
 
-function onInput(e) {
-  
-   debounce((e) => {
-          e.preventDefault()
-    searchQuery=refs.input.value
-        console.log(refs.input.value)
-        console.log('debounce')
-    return `${searchQuery}`
-     
-    },500)
- 
-  
+refs.input.addEventListener('input',  debounce(onInput,500))
+
+function onInput() {
+
+    searchQuery = refs.input.value;
+
+    fetchCountries(searchQuery)
+        .then((countries) => onResponse(countries));
 }
 
-const Countries = fetchCountries(searchQuery)
-console.log(Countries)
 
+//  1. вывести результат одной страницы
+
+//  2. вывести результат список стран от 2 до 10 штук
+
+//  3. если стран больше 10 - вывести "уточните параметры запроса"
+
+// проверка на количество стран в промисе------------------------------
+function onResponse (countries){
+    if (countries.length > 10) {
+     renderClarifyMessage(countries)
+    } else if (countries.length >= 2 && countries.length <= 10) {
+        renderCountriesMany(countries)
+    } else if (countries.length===1){
+        renderCountry(countries)
+    } else {
+       renderDefaultMessage()
+  }
+}
+// если больше 10 стран-----------------------------
+function renderClarifyMessage(countries) {
+
+    // очистить контент
+
+     alert({
+    text: 'Notice me, senpai!'
+     });
+    console.log('renderClarifyMessage')
+    console.log(countries)
+}
+
+// если от 2х до 10ти стран------------------------------
+function renderCountriesMany(countries) {
+    // очистить контент
+    // вывести список стран
+    console.log('renderCountriesMany')
+    console.log(countries)
+}
+// если одна страна------------------------------
+function renderCountry(countries) {
+    // очистить контент
+     // вывести картинку страны
+    
+    console.log('renderCountry')
+    console.log(countries)
+}
+// //если ни одной страны---------------------------------
+function renderDefaultMessage() {
+    // очистить контент
+   
+    console.log('Такой страны не существует, уточните поиск')
+    
+}
+
+
+
+
+//функция очистки контента-----------------------------
+
+function clearContent() {
+    refs.countryContainer.inner
+}
+
+// switch (true) {
+    
+//     case countries.length > 10:
+//         renderClarifyMessage(countries);
+//         break;
+//      case countries.length >= 2 && countries.length <= 10:
+//         renderCountriesMany(countries);
+//         break;
+//      case countries.length === 1:
+//         renderCountry(countries);
+//         break;
+//     default:
+//         renderDefaultMessage()
+
+// }
